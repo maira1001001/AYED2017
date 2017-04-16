c7e803a6e851f61acbe0c54bc161dddf76f93df5
Pangolins <maira.diaz.73@gmail.com>
2017-04-16 12:30:23 -0300

    add method "espejo()"
    espejo(): ArbolBinario<T> 
    This method returns the mirror of a binary tree

--- ArbolBinario/ArbolBinario.java
+++ ArbolBinario/ArbolBinario.java
@@ -56,5 +56,66 @@ public class ArbolBinario<T> {
 	public boolean esHoja() {
 		return this.getDatoRaiz() != null && this.getHijoIzquierdo().esVacio() && this.getHijoDerecho().esVacio();
 	}
+		
+	public int contarHojas(){
+		int cantHojas = 0;
+		if (this.esHoja()){
+			cantHojas++;
+		} else {
+			if (!this.getHijoIzquierdo().esVacio()){
+				cantHojas =+ this.getHijoIzquierdo().contarHojas();
+			}
+			if (!this.getHijoDerecho().esVacio()){
+				cantHojas =+ this.getHijoDerecho().contarHojas();
+			}
+		}
+		return cantHojas;
+	}
 
+	private void swapHijos(){
+		ArbolBinario<T> d = this.getHijoDerecho();
+		ArbolBinario<T> i = this.getHijoIzquierdo();
+		this.agregarHijoDerecho(i);
+		this.agregarHijoIzquierdo(d);
+	}
+	
+	public ArbolBinario<T> espejo(){
+		if (!this.esHoja()) {	
+			this.swapHijos();
+			if (!this.getHijoDerecho().esVacio()) this.getHijoDerecho().espejo();
+			if (!this.getHijoIzquierdo().esVacio()) this.getHijoIzquierdo().espejo();
+		}
+		return this;
+	}
+
+	public static void main(String args[]){
+		ArbolBinario<Integer> a = new ArbolBinario<Integer>(new Integer(7));
+		ArbolBinario<Integer> b = new ArbolBinario<Integer>(new Integer(15));
+		ArbolBinario<Integer> c = new ArbolBinario<Integer>(new Integer(20));
+		ArbolBinario<Integer> d = new ArbolBinario<Integer>(new Integer(25));
+		ArbolBinario<Integer> e = new ArbolBinario<Integer>(new Integer(30));
+		ArbolBinario<Integer> f = new ArbolBinario<Integer>(new Integer(36));
+		ArbolBinario<Integer> g = new ArbolBinario<Integer>(new Integer(44));
+		ArbolBinario<Integer> h = new ArbolBinario<Integer>(new Integer(47));
+		ArbolBinario<Integer> i = new ArbolBinario<Integer>(new Integer(50));
+		
+		//e RAIZ
+		e.agregarHijoIzquierdo(b); 
+		e.agregarHijoDerecho(g); 
+		b.agregarHijoIzquierdo(a);  
+		b.agregarHijoDerecho(c);
+		g.agregarHijoIzquierdo(f);
+		g.agregarHijoDerecho(i);
+		c.agregarHijoDerecho(d);
+		i.agregarHijoIzquierdo(h);
+		
+		
+		e.espejo();
+		
+		
+		
+		
+		
+		
+	}
 }
